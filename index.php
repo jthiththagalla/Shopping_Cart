4ccba5d2ad106746ead18f5366981572b9926128
jthiththagalla <thiththagalla.janeth@gmail.com>
2018-01-05 21:55:06 +0530

    2nd commit

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<html>
+    <title>Shoppping Cart</title>
+    <header> <h1>Shopping Cart</h1></header
+    <body>    
+        
+    </body>
+</html>
